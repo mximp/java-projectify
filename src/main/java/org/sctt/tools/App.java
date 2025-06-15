c39484040ef8d0980ac82ddb9d906d4808a6fb6b
mximp <mxim.petrov@gmail.com>
2025-06-15 08:48:11 +0300

    exec plugin

--- src/main/java/org/sctt/tools/App.java
+++ src/main/java/org/sctt/tools/App.java
@@ -5,11 +5,15 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.concurrent.Callable;
 
+/**
+ * Entry point class for the app.
+ */
 @Command(name = "jpfy", mixinStandardHelpOptions = true, version = "jpfy 0.1",
         description = "Turns a bunch of java files into proper project")
-class App implements Callable<Integer> {
+public final class App implements Callable<Integer> {
 
     /**
      * Source root folder.
@@ -19,10 +23,21 @@ class App implements Callable<Integer> {
 
     @Override
     public Integer call() {
-        System.out.println("Succeed in doing nothing for " + this.file);
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException("Not a source folder");
+        }
+
+        Arrays.stream(this.file.list((f, s) -> s.endsWith(".java"))).forEach(
+                System.out::println
+        );
+
         return 0;
     }
 
+    /**
+     * Main method.
+     * @param args Arguments
+     */
     public static void main(final String... args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
